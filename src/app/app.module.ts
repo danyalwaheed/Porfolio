071de432256709125ea8bb757b7d45258ecebc19
unknown <danyalwaheed152@gmail.comgit config --global user.name Danyal>
2023-07-29 15:19:35 +0400

    intial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ServiceComponent } from './service/service.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    ServiceComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IonicModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
